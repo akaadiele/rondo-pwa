12abd82cc9606116851f8a90b985032c2dc4e8be
akaadiele <akaadiele@gmail.com>
2025-02-25 19:31:03 +0000

    functionalities for pitch finder and pitch details

--- js/pitch-details.js
+++ js/pitch-details.js
@@ -1,39 +1,23 @@
+// Initiate 'viewPlace' function
 document.addEventListener("DOMContentLoaded", viewPlace);
 
-const urlSplit = document.URL.split('?place='); const place_id = urlSplit[1];
-
-// const apiURL = "https://maps.googleapis.com/maps/api/place/details/json";
-// const apiKey = "AIzaSyCbvT_-aAvCZGD1uR70C5CUVHCEh3UK4Yo";
-// const fullURL = apiURL + "?key=" + apiKey + "&place_id=" + place_id;
+// Get place_id from url
+const urlSplit = document.URL.split('?pitch='); const place_id = urlSplit[1];
 
 
-const apiURL = "https://places.googleapis.com/v1/places/";
-const apiKey = "AIzaSyCbvT_-aAvCZGD1uR70C5CUVHCEh3UK4Yo";
-const apiFields = "id,displayName,formattedAddress,internationalPhoneNumber,nationalPhoneNumber,location,googleMapsLinks,googleMapsUri,websiteUri";
-const fullURL = apiURL + place_id + "?fields=" + apiFields + "&key=" + apiKey;
-
-// ChIJw1WnzEhchkgReBxirHIzT0k
-// ?fields=
-// id,displayName,formattedAddress,internationalPhoneNumber,nationalPhoneNumber,location,googleMapsLinks,googleMapsUri,websiteUri
-// &key=
-// AIzaSyCbvT_-aAvCZGD1uR70C5CUVHCEh3UK4Yo
-
-const pitchInfoCard = document.getElementById("pitchInfo");
+const pitchInfoDiv = document.getElementById("pitchInfo");
 
 
+// Google Maps API to get place details
 function viewPlace() {
-    // // Fetch contents from the API response and format contents to display as HTML elements
-    console.log("url: " + fullURL)
-
-    // fetch(fullURL)
-    //     .then(x => x.text())
-    //     .then(y => console.log(y));
-
+    const apiURL = "https://places.googleapis.com/v1/places/";
+    const apiFields = "id,displayName,formattedAddress,internationalPhoneNumber,location,googleMapsLinks,rating";
+    const fullURL = apiURL + place_id + "?fields=" + apiFields + "&key=" + apiKey;
+    
+    // Fetch contents from the API response and format contents to display as HTML elements
     fetch(fullURL)
         .then(response => response.text())
         .then(result => {
-
-            // console.log("result: " + result);
 
             const resultJson = JSON.parse(result) || [];
             if (!resultJson) {
@@ -47,44 +31,73 @@ function viewPlace() {
             // Creating HTML elements
 
 
-            let pitchLat = resultJson.location.latitude;
-            let pitchLng = resultJson.location.longitude;
-
-            // Building list and list contents
+            // Building card and card contents
             const contentDiv = createNode('div');
-            contentDiv.setAttribute('class', 'card-body');
+            contentDiv.setAttribute('class', 'card-body p-3 darkBg fontColor1');
 
             const contentH5 = createNode('h5');
             contentH5.setAttribute('class', 'card-title');
             contentH5.innerHTML = resultJson.displayName.text;
 
-            const contentP = createNode('p');
-            contentP.setAttribute('class', 'card-text cardP');
-            
+            const contentP1 = createNode('p');
+            contentP1.setAttribute('class', 'card-text cardP');
+            contentP1.innerHTML = `<hr><i class="fa-solid fa-map-pin"></i>:&nbsp;&nbsp;` + resultJson.formattedAddress;
+
+            const contentP2 = createNode('p');
+            contentP2.setAttribute('class', 'card-text cardP');
             if (resultJson.internationalPhoneNumber) {
-            contentP.innerHTML = `<hr><i class="fa-solid fa-map-pin"></i>: ` + resultJson.formattedAddress + `<br><br><i class="fa-solid fa-phone"></i>: ` + resultJson.internationalPhoneNumber;
-        } else {
-            contentP.innerHTML = `<hr><i class="fa-solid fa-map-pin"></i>: ` + resultJson.formattedAddress + `<br>`
-        }
+                contentP2.innerHTML = `<i class="fa-solid fa-phone"></i>:&nbsp;&nbsp;` + resultJson.internationalPhoneNumber;
+            }
+
+            const contentP3 = createNode('p');
+            contentP3.setAttribute('class', 'card-text cardP');
+            contentP3.innerHTML = `<i class="fa-solid fa-star"></i>:&nbsp;&nbsp;` + resultJson.rating + `<hr>`;
+
 
             const contentA = createNode('a');
             contentA.setAttribute('href', resultJson.googleMapsLinks.placeUri);
-            contentA.setAttribute('class', 'btn links');
-            contentA.innerHTML = `<i class="fa-solid fa-map-location"></i>`;
+            contentA.setAttribute('class', 'btn btn-light links');
+            contentA.innerHTML = `<i class="fa-brands fa-google"></i>`;
 
 
             append(contentDiv, contentH5);
-            append(contentDiv, contentP);
+            append(contentDiv, contentP1);
+            append(contentDiv, contentP2);
+            append(contentDiv, contentP3);
             append(contentDiv, contentA);
-            append(pitchInfoCard, contentDiv);
+            append(pitchInfoDiv, contentDiv);
 
+            let pitchLat = resultJson.location.latitude;
+            let pitchLng = resultJson.location.longitude;
+            initMap(pitchLat, pitchLng)
+
+            // console.log("result: " + resultJson);
         })
         .catch(error => {
             // Custom error response
             // console.error("Search Error:", error);
-            console.log("ISearch Error: ", error);
-            console.log("url: " + fullURL)
+            // console.log("Search Error: ", error);
         });
 }
+// >>>
 
+
+
+// Define Map with center and pin at the selected pitch
+let map;
+async function initMap(pitchLatitude, pitchLongitude) {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: pitchLatitude, lng: pitchLongitude },
+        zoom: 12,
+        gestureHandling: "cooperative",
+    });
+
+    let currentMarker = new google.maps.Marker({
+        position: { lat: pitchLatitude, lng: pitchLongitude }
+    })
+    currentMarker.setMap(map);
+}
+// >>>
 
--- js/rondo-ui.js
+++ js/rondo-ui.js
@@ -1,7 +1,11 @@
+const apiKey = "AIzaSyCbvT_-aAvCZGD1uR70C5CUVHCEh3UK4Yo";
+
+
 function createNode(element) {
     // Function to create new HTML element
     return document.createElement(element);
 }
+
 
 function append(parent, child) {
     // Function to append HTML elements
